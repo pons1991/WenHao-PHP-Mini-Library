c44364be7c49619b884bf629d27d95f532d29133
Ponsundar <root@localhost.localdomain>
2016-08-20 14:56:54 -0400

    Changed the Const.php and Added commented for the better understanding.

--- Const.php
+++ Const.php
@@ -1,33 +1,31 @@
 <?php
-	// $DOMAIN_NAME = "http://bakhacheluxuries.com/leave/";
-	// $DATABASE_NAME = "bakhache_leave";
-    // $MAIN_PAGE = "/shell/dashboard/index.php";
-    
+   /* Database configurations */
     $CONNECTION_CONFIG_NAME = "/config/connectionstring.json";
+
+    /* Specifying the index page */
     $MAIN_PAGE = "/shell/dashboard/index.php";
     
     $BRING_FORWARD_EXPIRE_DATE_DAYS = 31;
     $BRING_FORWARD_EXPIRE_DATE_MONTH = 3;
     
+    /* Page index settings */
     $DEFAULT_PAGE_INDEX = 1;
     $DEFAULT_MAX_PAGE_INDEX = 999;
+    /* Page size settings */
     $PAGE_SIZE = 15;
     $PAGE_SINGLE_ITEM = 1;
     
     $AUTO_LEAVESTATUS = 2;
+
+    /* System user configurations */
     $SYSTEM_USER_ID = 1;
     $SYSTEM_USER_EMAIL = 'admin';
     
-    //development configuration
-	$DOMAIN_NAME = "http://localhost:8888/whLibrary/";
+    /* development configuration, You have to change in production environment. */
+    $DOMAIN_NAME = "http://localhost:8888/whLibrary/";
     $FOLDER_PATH = "/whLibrary";
-	$DATABASE_NAME = "whMiniLibrary";
+    $DATABASE_NAME = "whMiniLibrary";
     
-    //production configuration
-    /*
-    $DOMAIN_NAME = "http://bakhacheluxuries.com/leave/";
-	$DATABASE_NAME = "bakhache_leave";
-    */
     
     
 ?>
